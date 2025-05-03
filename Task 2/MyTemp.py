8bdfe8d3420c7de92008470758810224156e5a86
Youssef Haitham <90577356+Youssef-MokhtarDS@users.noreply.github.com>
2025-05-03 19:17:31 +0300

    Add files via upload

---
+++ Task 2/MyTemp.py
@@ -0,0 +1,7 @@
+def fahrenheit_to_celsius(fahrenheit):
+    """Convert Fahrenheit to Celsius"""
+    return (fahrenheit - 32) * 5 / 9
+
+def celsius_to_fahrenheit(celsius):
+    """Convert Celsius to Fahrenheit"""
+    return (celsius * 9 / 5) + 32
